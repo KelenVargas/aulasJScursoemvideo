959398a529007a435c2ada3c2f75264b5b261b86
Kelen Vargas <kelenvargas.senac@gmail.com>
2022-06-23 08:21:08 -0300

    Adicionando os excercicios da aula 11 e 12 que fiz

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 50.5
+console.log (`a velocidade do seu carro é ${vel} km/h`)
+if(vel>60){// condição simples
+    
+    console.log(`Você ultrapasou a velocidade permetida! Multado!!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!!`)
+
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`Vivendo no ${pais}`)
+if( pais != 'Brasil'){
+    console.log(`Você é Estrangeiro!!`)
+} else{
+    console.log(`Você é Brasileiro!!`)
+
+}
